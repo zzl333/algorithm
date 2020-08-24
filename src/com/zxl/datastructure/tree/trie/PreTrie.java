4f86a064a358929522bbf2364614326f936694a8
zzl333 <1710643798@qq.com>
2020-08-24 21:51:57 +0800

    最大异或值

---
+++ src/com/zxl/datastructure/tree/trie/PreTrie.java
@@ -0,0 +1,58 @@
+package com.zxl.datastructure.tree.trie;
+
+import java.util.HashMap;
+
+/**
+ * 给定一个非空数组，数组中元素为 a0, a1, a2, … , an-1，其中 0 ≤ ai < 231 。
+ *
+ * 找到 ai 和aj 最大的异或 (XOR) 运算结果，其中0 ≤ i,  j < n 。
+ *
+ * 你能在O(n)的时间解决这个问题吗？
+ *
+ */
+public class PreTrie {
+    class TrieNode {
+        HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+        public TrieNode() {}
+    }
+
+    public int findMaximumXOR(int[] nums) {
+        int maxNum = nums[0];
+        for(int num : nums) maxNum = Math.max(maxNum, num);
+        int L = (Integer.toBinaryString(maxNum)).length();
+
+        int n = nums.length, bitmask = 1 << L;
+        String [] strNums = new String[n];
+        for(int i = 0; i < n; ++i) {
+            strNums[i] = Integer.toBinaryString(bitmask | nums[i]).substring(1);
+        }
+
+        TrieNode trie = new TrieNode();
+        int maxXor = 0;
+        for (String num : strNums) {
+            TrieNode node = trie, xorNode = trie;
+            int currXor = 0;
+            for (Character bit : num.toCharArray()) {
+                if (node.children.containsKey(bit)) {
+                    node = node.children.get(bit);
+                } else {
+                    TrieNode newNode = new TrieNode();
+                    node.children.put(bit, newNode);
+                    node = newNode;
+                }
+
+                Character toggledBit = bit == '1' ? '0' : '1';
+                if (xorNode.children.containsKey(toggledBit)) {
+                    currXor = (currXor << 1) | 1;
+                    xorNode = xorNode.children.get(toggledBit);
+                } else {
+                    currXor = currXor << 1;
+                    xorNode = xorNode.children.get(bit);
+                }
+            }
+            maxXor = Math.max(maxXor, currXor);
+        }
+
+        return maxXor;
+    }
+}
