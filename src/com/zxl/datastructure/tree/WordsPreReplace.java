ef289204e091470f92bce8d975828bd05c0a8c9e
zzl333 <1710643798@qq.com>
2020-08-26 20:12:42 +0800

    修正乱码

--- src/com/zxl/datastructure/tree/WordsPreReplace.java
+++ src/com/zxl/datastructure/tree/WordsPreReplace.java
@@ -3,19 +3,11 @@ package com.zxl.datastructure.tree;
 import java.util.List;
 
 /**
- * ���룺dict(�ʵ�) = ["cat", "bat", "rat"] sentence(����) = "the cattle was rattled by the battery"
- *�����"the cat was rat by the bat"
- *�ʸ��滻
- *�˴����ֵ�����ʽ��Map<�ֵ���, String> String Ϊ���ַ���
- *���sentence��ǰ׺ƥ�䵽��ǰ׺����Ҷ�ӽڵ㣬��ô����ǰ׺����Ҷ�ӽڵ��滻sentence�е��ַ���
- * @author zxl
  *
  */
 public class WordsPreReplace {
     public String replaceWords(List<String> roots, String sentence) {
-    	//�ֶ��ֵ���
         TrieNode trie = new TrieNode();
-        //�����ֵ��� wordΪ�ַ���
         for (String root: roots) {
             TrieNode cur = trie;
             for (char letter: root.toCharArray()) {
@@ -44,7 +36,6 @@ public class WordsPreReplace {
     }
 }
 
-//�ֶ��ֵ���
 class TrieNode {
     TrieNode[] children;
     String word;
