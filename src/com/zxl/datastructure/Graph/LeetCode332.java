4bb40752e8a551c2fa0c9236bb13eb5a75b761b4
zzl333 <1710643798@qq.com>
2020-08-27 20:47:00 +0800

    图论旅行家问题，半欧拉图

---
+++ src/com/zxl/datastructure/Graph/LeetCode332.java
@@ -0,0 +1,36 @@
+package com.zxl.datastructure.Graph;
+
+
+import java.util.*;
+
+/**
+ * 给定一个机票的字符串二维数组 [from, to]，子数组中的两个成员分别表示飞机出发和降落的机场地点，
+ * 对该行程进行重新规划排序。所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，
+ * 所以该行程必须从 JFK 开始。
+ *
+ */
+public class LeetCode332 {
+    Map<String, PriorityQueue<String>> map = new HashMap<String, PriorityQueue<String>>();
+    List<String> itinerary = new LinkedList<String>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        for (List<String> ticket : tickets) {
+            String src = ticket.get(0), dst = ticket.get(1);
+            if (!map.containsKey(src)) {
+                map.put(src, new PriorityQueue<String>());
+            }
+            map.get(src).offer(dst);
+        }
+        dfs("JFK");
+        Collections.reverse(itinerary);
+        return itinerary;
+    }
+
+    public void dfs(String curr) {
+        while (map.containsKey(curr) && map.get(curr).size() > 0) {
+            String tmp = map.get(curr).poll();
+            dfs(tmp);
+        }
+        itinerary.add(curr);
+    }
+}
